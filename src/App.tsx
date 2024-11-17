94eb816e70e89452706e7614da6fa6c17b859a79
J-Breno <joao.breno85@hotmail.com>
2024-11-16 23:54:21 -0300

    Links withuot full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Bookkeeper!</h1>
-  )
+    <nav
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Link to="/invoices">Invoices</Link> |{" "}
+      <Link to="/expenses">Expenses</Link>
+    </nav>
+  );
 }
 
-export default App
+export default App;
