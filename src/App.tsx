0e2d97fb23a33a26db699688cbd2ab1bcb8965b6
J-Breno <joao.breno85@hotmail.com>
2024-11-17 00:43:32 -0300

    Listing invoices

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 function App() {
-
   return (
-    <nav
-      style={{
-        borderBottom: "solid 1px",
-        paddingBottom: "1rem",
-      }}
-    >
-      <Link to="/invoices">Invoices</Link> |{" "}
-      <Link to="/expenses">Expenses</Link>
-    </nav>
+    <>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,10 @@ import Invoices from "./routes/Invoices/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="expenses" element={<Expenses />} />
-    <Route path="invoices" element={<Invoices />} />
-  </Routes>
-</BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>
 );
